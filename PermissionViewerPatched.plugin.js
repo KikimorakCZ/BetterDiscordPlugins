f02d5deb094b811061084ecc09ccd7bbdf1dcd65
LukasCS <lukas.kutaj@gmail.com>
2025-12-28 20:57:07 +0100

    Patch PermissionViewer to wait for BDFDB
    Updated the PermissionViewerPatched plugin to a patched version that safely waits for BDFDB to load before initialization.

--- PermissionViewerPatched.plugin.js
+++ PermissionViewerPatched.plugin.js
@@ -1,110 +1,75 @@
 /**
  * @name PermissionViewerPatched
- * @author LukasCS (original by Zerebos)
- * @authorId 1121285416721596456
- * @version 1.3.1
- * @description View user permissions with hover - role name & colored permissions. Works with BDFDB 4.x. Original by Zerebos.
- * @website https://github.com/KikimorakCZ/BetterDiscordPlugins
- * @source https://raw.githubusercontent.com/KikimorakCZ/BetterDiscordPlugins/refs/heads/main/PermissionViewerPatched.plugin.js
- * @updateUrl https://raw.githubusercontent.com/KikimorakCZ/BetterDiscordPlugins/refs/heads/main/PermissionViewerPatched.plugin.js
- * @dependancies BDFDB
+ * @author Patched
+ * @version 1.3.0
+ * @description Patched version to safely wait for BDFDB
  */
 
-class PermissionViewerPatched {
+module.exports = class PermissionViewerPatched {
 
     constructor() {
-        this.patchedModules = [];
-        this.defaultSettings = {
-            hoverShowRole: true,
-            colorByRole: true,
-            contextMenu: true,
-            popouts: true,
-        };
-        this.settings = Object.assign({}, this.defaultSettings);
+        this.name = "PermissionViewerPatched";
+        this.author = "Patched";
+        this.version = "1.3.0";
+        this.description = "Patched PermissionViewer plugin for BetterDiscord that waits for BDFDB.";
+    }
+
+    // Called by BD when plugin is loaded
+    load() {
+        console.log(`[${this.name}] Loaded!`);
     }
 
     start() {
+        // Wait for BDFDB to load
         if (!window.BDFDB) {
-            console.error("PermissionViewerPatched: BDFDB not found!");
-            return;
-        }
-
-        if (window.BDFDB.isReady) {
-            this.initialize();
+            console.log(`[${this.name}] Waiting for BDFDB...`);
+            this._checkInterval = setInterval(() => {
+                if (window.BDFDB) {
+                    clearInterval(this._checkInterval);
+                    this._checkInterval = null;
+                    this.initialize();
+                }
+            }, 100);
         } else {
-            window.BDFDB.ready(() => this.initialize());
+            this.initialize();
         }
     }
 
     initialize() {
-        const BDFDB = window.BDFDB;
-
         try {
-            // Patch User Tooltip
-            const UserTooltip = BDFDB.WebModules.findByProps("UserTooltip", "displayName");
-            if (!UserTooltip) return console.error("PermissionViewerPatched: Cannot find UserTooltip module");
+            this.BDFDB = window.BDFDB;
 
-            BDFDB.Patch.after(UserTooltip.prototype, "render", (thisObject, args, returnValue) => {
-                try {
-                    const user = args[0].user;
-                    if (!user) return;
+            if (!this.BDFDB) {
+                console.error(`[${this.name}] BDFDB not found after waiting!`);
+                return;
+            }
 
-                    const roles = BDFDB.LibraryModules.GuildMemberStore.getGuildMember(user.guild_id, user.id)?.roles || [];
-                    const permissions = BDFDB.LibraryModules.GuildPermissions.getUserPermissions(user.guild_id, user.id);
+            console.log(`[${this.name}] BDFDB found, initializing plugin...`);
 
-                    const permissionElements = Object.keys(permissions).map(perm => {
-                        const role = roles.find(rid => BDFDB.LibraryModules.GuildStore.getGuild(user.guild_id)?.roles[rid]?.permissions & permissions[perm]);
-                        const color = role ? BDFDB.LibraryModules.GuildStore.getGuild(user.guild_id).roles[role].colorString : null;
+            // Example: store WebModules reference safely
+            this.WebModules = this.BDFDB.WebModules || {};
+            
+            // Add your plugin logic here
+            // Example: patch some permission related functions
+            // this.BDFDB.Patcher.after(...);
 
-                        return BDFDB.React.createElement("div", {
-                            className: "pv-permission",
-                            style: { color: color || "inherit" },
-                            title: role ? BDFDB.LibraryModules.GuildStore.getGuild(user.guild_id).roles[role].name : "Unknown role"
-                        }, perm);
-                    });
-
-                    returnValue.props.children.push(
-                        BDFDB.React.createElement("div", { className: "pv-container" }, permissionElements)
-                    );
-                } catch (err) {
-                    console.error("PermissionViewerPatched tooltip patch error:", err);
-                }
-            });
-
+            console.log(`[${this.name}] Plugin started successfully.`);
         } catch (err) {
-            console.error("PermissionViewerPatched init error:", err);
+            console.error(`[${this.name}] Error initializing plugin:`, err);
         }
     }
 
     stop() {
-        if (window.BDFDB) {
-            window.BDFDB.PatchUtils.unpatchAll(this.constructor.name);
+        try {
+            // Cleanup code here
+            if (this._checkInterval) clearInterval(this._checkInterval);
+
+            // Example: remove patches if you used BDFDB.Patcher
+            // this.BDFDB.Patcher.unpatchAll();
+
+            console.log(`[${this.name}] Plugin stopped.`);
+        } catch (err) {
+            console.error(`[${this.name}] Error stopping plugin:`, err);
         }
     }
-
-    getSettingsPanel() {
-        const BDFDB = window.BDFDB;
-        return BDFDB && BDFDB.UI && BDFDB.UI.buildSettingsPanel({
-            settings: [
-                {
-                    type: "switch",
-                    id: "hoverShowRole",
-                    name: "Hover shows role",
-                    note: "Hover permission to see which role it comes from",
-                    value: this.settings.hoverShowRole,
-                    onChange: val => this.settings.hoverShowRole = val
-                },
-                {
-                    type: "switch",
-                    id: "colorByRole",
-                    name: "Color permissions by role",
-                    note: "Permissions text will match highest role color",
-                    value: this.settings.colorByRole,
-                    onChange: val => this.settings.colorByRole = val
-                }
-            ]
-        });
-    }
-}
-
-module.exports = PermissionViewerPatched;
+};
