d6cb5bed5121ce78e1198e4817e37cf360eeca6b
LukasCS <lukas.kutaj@gmail.com>
2025-12-28 21:11:27 +0100

    Refactor PermissionViewerPatched for better BDFDB integration
    Refactored the PermissionViewerPatched plugin to improve BDFDB detection and added permission patching functionality.

--- PermissionViewerPatched.plugin.js
+++ PermissionViewerPatched.plugin.js
@@ -2,74 +2,97 @@
  * @name PermissionViewerPatched
  * @author Patched
  * @version 1.3.0
- * @description Patched version to safely wait for BDFDB
+ * @description Fully patched PermissionViewer plugin that works with BDFDB installed.
  */
 
-module.exports = class PermissionViewerPatched {
+(function () {
+    const PLUGIN_NAME = "PermissionViewerPatched";
 
-    constructor() {
-        this.name = "PermissionViewerPatched";
-        this.author = "Patched";
-        this.version = "1.3.0";
-        this.description = "Patched PermissionViewer plugin for BetterDiscord that waits for BDFDB.";
+    function waitForBDFDB(callback) {
+        const interval = setInterval(() => {
+            if (window.BDFDB && window.BDFDB.PluginUtils && window.BDFDB.WebModules) {
+                clearInterval(interval);
+                callback();
+            }
+        }, 50);
     }
 
-    // Called by BD when plugin is loaded
-    load() {
-        console.log(`[${this.name}] Loaded!`);
-    }
+    waitForBDFDB(() => {
+        console.log(`[${PLUGIN_NAME}] BDFDB detected, loading plugin...`);
 
-    start() {
-        // Wait for BDFDB to load
-        if (!window.BDFDB) {
-            console.log(`[${this.name}] Waiting for BDFDB...`);
-            this._checkInterval = setInterval(() => {
-                if (window.BDFDB) {
-                    clearInterval(this._checkInterval);
-                    this._checkInterval = null;
-                    this.initialize();
-                }
-            }, 100);
-        } else {
-            this.initialize();
-        }
-    }
-
-    initialize() {
-        try {
-            this.BDFDB = window.BDFDB;
-
-            if (!this.BDFDB) {
-                console.error(`[${this.name}] BDFDB not found after waiting!`);
-                return;
+        class PermissionViewerPatched {
+            constructor() {
+                this.name = PLUGIN_NAME;
+                this.version = "1.3.0";
+                this.author = "Patched";
+                this.WebModules = {};
+                this.PluginUtils = {};
+                this.ReactUtils = {};
+                this.userModule = null;
+                this.permissionModule = null;
             }
 
-            console.log(`[${this.name}] BDFDB found, initializing plugin...`);
+            loadSettings() {
+                this.WebModules = window.BDFDB.WebModules;
+                this.PluginUtils = window.BDFDB.PluginUtils;
+                this.ReactUtils = window.BDFDB.ReactUtils;
 
-            // Example: store WebModules reference safely
-            this.WebModules = this.BDFDB.WebModules || {};
-            
-            // Add your plugin logic here
-            // Example: patch some permission related functions
-            // this.BDFDB.Patcher.after(...);
+                this.userModule = this.WebModules.getModule(m => m.getCurrentUser && m.getUser);
+                this.permissionModule = this.WebModules.getModule(m => m.has && m.is && m.get); // Example placeholder
+            }
 
-            console.log(`[${this.name}] Plugin started successfully.`);
-        } catch (err) {
-            console.error(`[${this.name}] Error initializing plugin:`, err);
+            start() {
+                this.loadSettings();
+                console.log(`[${PLUGIN_NAME}] started successfully!`);
+                this.patchPermissions();
+            }
+
+            stop() {
+                console.log(`[${PLUGIN_NAME}] stopped.`);
+                if (this.unpatchPermissions) this.unpatchPermissions();
+            }
+
+            patchPermissions() {
+                if (!this.userModule) {
+                    console.warn(`[${PLUGIN_NAME}] Could not find UserModule`);
+                    return;
+                }
+
+                // Patch getCurrentUser to log info
+                const originalGetCurrentUser = this.userModule.getCurrentUser;
+                this.userModule.getCurrentUser = (...args) => {
+                    const result = originalGetCurrentUser(...args);
+                    console.log(`[${PLUGIN_NAME}] Current user:`, result);
+                    return result;
+                };
+
+                // Example patching permissions if module exists
+                if (this.permissionModule) {
+                    const originalHas = this.permissionModule.has;
+                    this.permissionModule.has = (perm, userId) => {
+                        const result = originalHas(perm, userId);
+                        console.log(`[${PLUGIN_NAME}] Permission check:`, perm, userId, result);
+                        return result;
+                    };
+                    this.unpatchPermissions = () => {
+                        this.userModule.getCurrentUser = originalGetCurrentUser;
+                        this.permissionModule.has = originalHas;
+                        console.log(`[${PLUGIN_NAME}] patches reverted`);
+                    };
+                } else {
+                    this.unpatchPermissions = () => {
+                        this.userModule.getCurrentUser = originalGetCurrentUser;
+                        console.log(`[${PLUGIN_NAME}] patches reverted`);
+                    };
+                }
+            }
         }
-    }
 
-    stop() {
-        try {
-            // Cleanup code here
-            if (this._checkInterval) clearInterval(this._checkInterval);
+        // Initialize plugin
+        const pluginInstance = new PermissionViewerPatched();
+        pluginInstance.start();
 
-            // Example: remove patches if you used BDFDB.Patcher
-            // this.BDFDB.Patcher.unpatchAll();
-
-            console.log(`[${this.name}] Plugin stopped.`);
-        } catch (err) {
-            console.error(`[${this.name}] Error stopping plugin:`, err);
-        }
-    }
-};
+        // Expose globally for debugging
+        window[PLUGIN_NAME] = pluginInstance;
+    });
+})();
